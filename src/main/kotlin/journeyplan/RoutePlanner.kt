ee9ac143ebeb1681ed7caef86ae3383c7064350a
lyonely <z.y.f.9955@gmail.com>
2020-11-24 23:33:47 +0800

    Completed compulsory portions

--- src/main/kotlin/journeyplan/RoutePlanner.kt
+++ src/main/kotlin/journeyplan/RoutePlanner.kt
@@ -1,3 +1,60 @@
 package journeyplan
 
 // Add your code for the route planner in this file.
+
+class SubwayMap(private val map: List<Segment>) {
+    fun routesFrom(origin: Station, destination: Station): List<Route> {
+        val listOfRoutes: MutableList<Route> = mutableListOf<Route>()
+        val visitedStack: MutableList<Station> = mutableListOf(origin)
+        fun routeMaker(
+            pivot: Station,
+            destination1: Station,
+            visited: MutableList<Station>,
+            path: MutableList<Segment>
+        ) {
+            visited.add(pivot)
+            for (segment in map.filter { it.station1 == pivot }.filter { it.station2 !in visited }) {
+                if (segment.station2 == destination1) {
+                    listOfRoutes.add(Route(path + segment))
+                } else {
+                    routeMaker(segment.station2, destination1, visited, (path.plus(segment) as MutableList<Segment>))
+                }
+            }
+        }
+        routeMaker(origin, destination, visitedStack, mutableListOf<Segment>())
+        return listOfRoutes
+    }
+}
+
+fun main() {
+    val map = londonUndergound()
+    val routes = map.routesFrom(bakerStreet, lancasterGate)
+    for (route in routes) {
+        println(route)
+    }
+}
+
+val bakerlooLine = Line("Bakerloo")
+val centralLine = Line("Central")
+
+val bakerStreet = Station("Baker Street")
+val regentsPark = Station("Regent's Park")
+val oxfordCircus = Station("Oxford Circus")
+val bondStreet = Station("Bond Street")
+val marbleArch = Station("Marble Arch")
+val lancasterGate = Station("Lancaster Gate")
+
+fun londonUndergound(): SubwayMap {
+
+    return SubwayMap(
+        listOf(
+            Segment(bakerStreet, regentsPark, bakerlooLine, 3),
+            Segment(regentsPark, bakerStreet, bakerlooLine, 4),
+            Segment(regentsPark, oxfordCircus, bakerlooLine, 2),
+            Segment(oxfordCircus, regentsPark, bakerlooLine, 3),
+            Segment(oxfordCircus, bondStreet, centralLine, 5),
+            Segment(bondStreet, marbleArch, centralLine, 4),
+            Segment(marbleArch, lancasterGate, centralLine, 3)
+        )
+    )
+}
--- src/main/kotlin/journeyplan/TravelModel.kt
+++ src/main/kotlin/journeyplan/TravelModel.kt
@@ -1,3 +1,56 @@
 package journeyplan
 
 // Add your code for modelling public transport networks in this file.
+
+class Station(val name: String) {
+    override fun toString(): String = name
+}
+
+class Line(val name: String) {
+    override fun toString(): String = "$name Line"
+}
+
+class Segment(
+    val station1: Station,
+    val station2: Station,
+    val line: Line,
+    val travelTime: Int
+)
+
+class Route(val route: List<Segment>) {
+    fun duration(): Int {
+        var output = 0
+        for (segment in route) {
+            output += segment.travelTime
+        }
+        return output
+    }
+    fun numChanges(): Int {
+        val lines = mutableListOf<Line>()
+        for (segment in route) {
+            lines.add(segment.line)
+        }
+        return (lines.distinct().size - 1)
+    }
+    override fun toString(): String {
+        var origin = route.first().station1
+        var destination = route.last().station2
+        var line = route.first().line
+        val output = StringBuilder()
+        val totalDuration = duration()
+        val numberChanges = numChanges()
+        output.append("$origin to $destination - $totalDuration minutes, $numberChanges changes\n")
+        for (segment in route) {
+            if (segment.line == line) {
+                destination = segment.station2
+            } else if (segment.line != line) {
+                output.append(" - $origin to $destination by $line\n")
+                origin = destination
+                destination = segment.station2
+                line = segment.line
+            }
+        }
+        output.append(" - $origin to $destination by $line")
+        return output.toString()
+    }
+}
